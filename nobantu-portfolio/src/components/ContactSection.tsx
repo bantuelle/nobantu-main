e758dc94072b0f2d7c00c68b6b75a97ed4503930
bantu_00 <48910078+bantuelle@users.noreply.github.com>
2025-12-06 13:57:32 +0200

    Update ContactSection.tsx

--- nobantu-portfolio/src/components/ContactSection.tsx
+++ nobantu-portfolio/src/components/ContactSection.tsx
@@ -30,12 +30,12 @@ const ContactSection = () => {
 
           <div className="flex flex-col sm:flex-row gap-4 justify-center mb-12">
             <Button variant="hero" size="xl" asChild>
-              <a href="mailto:ndlovunobantu@gmail.com" className="group">
-                <Mail className="w-5 h-5" />
-                hello@nobantundlovu.co.za
-                <ArrowRight className="w-5 h-5 group-hover:translate-x-1 transition-transform" />
-              </a>
-            </Button>
+  <a href="mailto:ndlovunobantu@gmail.com" className="group">
+    <Mail className="w-5 h-5" />
+    <span className="text-sm">hello@nobantundlovu.co.za</span>
+    <ArrowRight className="w-5 h-5 group-hover:translate-x-1 transition-transform" />
+  </a>
+</Button>
           </div>
 
           {/* Contact Info */}
